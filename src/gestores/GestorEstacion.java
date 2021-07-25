dcdbc58de5c47c25bdbf8d96dddb882a6d0159c9
FedericoQuarin <federicoquarin2@gmail.com>
2021-07-25 19:33:26 -0300

    Metodos añadidos a Ruta, Estacion y sus gestores

--- src/gestores/GestorEstacion.java
+++ src/gestores/GestorEstacion.java
@@ -3,15 +3,23 @@ package gestores;
 import java.awt.Point;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
 import clases.Estacion;
+import clases.Ruta;
 
 public class GestorEstacion {
 	private List<Estacion> estaciones;
 	private static GestorEstacion gestor;
+	private GestorRuta gestorRutas;
 	
 	private GestorEstacion() {
 		estaciones = new ArrayList<>();
+		gestorRutas = GestorRuta.getInstance();
 	}
 	
 	public static GestorEstacion getInstance() {
@@ -29,4 +37,56 @@ public class GestorEstacion {
 	public void agregarEstacion(String i, String n, LocalTime hA, LocalTime hC, Point pos) {
 		estaciones.add(new Estacion(i, n, hA, hC, pos));
 	}
+	
+	public List<Estacion> getEstacionesAccesibles(Estacion estacion) {
+		List<Estacion> resultado = new ArrayList<>();
+		HashSet<Estacion> marcados = new HashSet<>();
+		List<Estacion> adyacentes = this.getAdyacentes(estacion);
+		
+		marcados.add(estacion);
+		
+		for (Estacion e : adyacentes) {
+			if (!marcados.contains(e)) {
+				resultado.addAll(getEstacionesAccesiblesAux(e, marcados));
+			}
+		}
+		
+		return resultado;
+	}
+	
+	private List<Estacion> getEstacionesAccesiblesAux(Estacion estacion, HashSet<Estacion> marcados) {
+		Stack<Estacion> pendientes = new Stack<>();
+		List<Estacion> resultado = new ArrayList<>();
+		
+		marcados.add(estacion);
+		pendientes.push(estacion);
+		
+		while (!pendientes.isEmpty()) {
+			Estacion e = pendientes.pop();
+			resultado.add(e);
+			List<Estacion> adyacentes = this.getAdyacentes(e);
+			for (Estacion e2 : adyacentes) {
+				if (!marcados.contains(e2)) {
+					marcados.add(e2);
+					pendientes.push(e2);
+				}
+			}
+		}
+		
+		
+		return resultado;
+		
+	}
+	
+	public List<Estacion> getAdyacentes(Estacion estacion) {
+		List<Ruta> rutas = gestorRutas.getRutas();
+		List<Estacion> resultado = new ArrayList<>();
+		
+		for (Ruta r : rutas) {
+			if (r.getOrigen().equals(estacion) && !resultado.contains(r.getOrigen())) resultado.add(r.getDestino());
+			
+		}
+		return resultado;
+	}
+
 }
