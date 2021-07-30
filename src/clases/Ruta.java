60d3b2b33d68e863f527fd2a8fb29bcb8f7d8039
Valenreynoso17 <valenreynoso17@gmail.com>
2021-07-29 21:56:55 -0300

    creo que se arreglo

---
+++ src/clases/Ruta.java
@@ -0,0 +1,68 @@
+package clases;
+import java.awt.Color;
+import java.util.Random;
+
+import enums.EstadoRuta;
+
+public class Ruta {
+
+	private Estacion origen;
+	private Estacion destino;
+	private Integer distancia;
+	private Integer duracion;
+	private Integer cantMaxPasajeros;
+	private EstadoRuta estado;
+	private double costo;	
+	
+	public Ruta(Estacion estacionOrigen, Estacion estacionDestino,Integer distancia, Integer duracion, Integer cantMaxPasajeros, EstadoRuta estado, double costo) {
+		this.origen = estacionOrigen;
+		this.destino = estacionDestino;
+		this.distancia = distancia;
+		this.duracion = duracion;
+		this.cantMaxPasajeros = cantMaxPasajeros;
+		this.estado = estado;
+		this.costo = costo;
+	}
+	
+	public Boolean activa() {
+		return estado == EstadoRuta.ACTIVA;
+	}
+	
+	public Estacion getOrigen() {
+		return origen;
+	}
+	
+	public Estacion getDestino() {
+		return destino;
+	}
+	
+	public Double getCosto() {
+		return this.costo;
+	}
+	
+	public Integer getDistancia() {
+		return this.distancia;
+	}
+	
+	public Integer getDuracion() {
+		return this.duracion;
+	}
+
+	// Hay que cambiarlo
+	public String getNombreLinea() {
+		return "Linea Colectivo 16";
+	}
+	
+	// Capaz lo termine sacando - FEDE
+	public Color getColorLinea() {
+		return Color.RED;
+	}
+	
+	// Para probar cositas. Hay que cambiarlo
+	public String linea() {
+		return "Linea: " + new Random().nextInt();
+	}
+	
+
+	
+}
