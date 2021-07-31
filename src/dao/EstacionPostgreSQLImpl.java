1b376013920fd993249f5ad35948d53f1b9f183c
FedericoQuarin <federicoquarin2@gmail.com>
2021-07-30 21:41:33 -0300

    Avances buscar en dao

--- src/dao/EstacionPostgreSQLImpl.java
+++ src/dao/EstacionPostgreSQLImpl.java
@@ -1,59 +1,63 @@
 package dao;
 
+import java.awt.Point;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import clases.Estacion;
 import clases.Recorrido;
+import enums.EstadoEstacion;
 
 public class EstacionPostgreSQLImpl implements EstacionDAO {
 	private String ip, port, usr, psw;
 	
-	@Override
-	public List<Estacion> buscar() {
+	public EstacionPostgreSQLImpl() {
 		// TODO Cambiar datos si hace falta
 		this.ip = "localhost";
 		this.port = "5432";
 		this.usr = "postgres";
 		this.psw = "DarkSouls";
+	}
+
+	@Override
+	public List<Estacion> buscar() {
+		List<Estacion> estaciones = new ArrayList<>();
+		String consulta = "SELECT * "
+						+ "FROM died.estacion;"	;
 		
 		Connection conn = null;
 		PreparedStatement st = null;
 		ResultSet rs = null;
+		
 		try {
 			Class.forName("org.postgresql.Driver");
 			conn = DriverManager.getConnection("jdbc:postgresql://"+ ip + ":" + port + "/", usr, psw);
 			
 			// Se realiza una consulta para encontrar el id correspondiente al siguiente recorrido
-			st = conn.prepareStatement(obtenerID);
+			st = conn.prepareStatement(consulta);
 			rs = st.executeQuery();
-			rs.next();
-			id = rs.getInt("max");
-			if (rs.wasNull()) id = 1;
-			else id += 1;
-			rs.close();
-			st.close();
 			
-			
-			st = conn.prepareStatement(insercionRecorrido + insercionBoleto);
-			st.setInt(1, id);
-			st.setInt(2, recorrido.getDistancia());
-			st.setInt(3, recorrido.getDuracion());
-			st.setDouble(4, recorrido.getCosto());
-			st.setInt(5, Integer.parseInt(recorrido.getOrigen().getId()));
-			st.setInt(6, 2);
-			st.setInt(7, boleto.getNroBoleto());
-			st.setString(8, boleto.getNombreCliente());
-			st.setString(9, boleto.getCorreoCliente());
-			System.out.println(boleto.getFechaVenta());
-			st.setDate(10, Date.valueOf(boleto.getFechaVenta()));
-			st.setInt(11, id);		
-			st.executeUpdate();
+			while(!rs.next()) {
+				String id = String.valueOf(rs.getInt("id"));
+				String nombre = rs.getString("nombre");
+				String estado = rs.getString("estado");
+				LocalTime horaApertura = rs.getTime("horarioapertura").toLocalTime();
+				LocalTime horaCierre = rs.getTime("horariocierre").toLocalTime();
+				Point posicion = new Point();
+				posicion.x = rs.getInt("posicion_x");
+				posicion.y = rs.getInt("posicion_y");
+				
+				// TODO falta agregarle que busque los mantenimientos
+				if (estado.equals("OPERATIVA")) estaciones.add(new Estacion(id, nombre, horaApertura, horaCierre, posicion, EstadoEstacion.OPERATIVA, null));
+				else estaciones.add(new Estacion(id, nombre, horaApertura, horaCierre, posicion, EstadoEstacion.EN_MANTENIMIENTO, null));
+			}
 			
 		} 
 		catch (ClassNotFoundException e) {
@@ -88,6 +92,8 @@ public class EstacionPostgreSQLImpl implements EstacionDAO {
 				}
 			}	
 		}
+		
+		return estaciones;
 	}
 
 	@Override
